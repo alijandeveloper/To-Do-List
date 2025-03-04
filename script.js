4cbc6be5360f778578f5dfaeebd151f9a92800ac
Ali Jan <alijandeveloper15@gmail.com>
2025-03-04 00:53:25 -0800

    start functionality

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const taskInput = document.getElementById("taskInput");
+    const addTaskButton = document.getElementById("addTask");
+    const taskList = document.getElementById("taskList");
+    const filters = document.querySelectorAll(".filter-btn");
